a3733e2df89ce34847d4c14d6df0c90480cc962d
Ruben Bakker <ruben@uncomplex.ch>
2025-12-21 10:17:03 +0100

    codecrafters submit [skip ci]

--- src/response.zig
+++ src/response.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const gzip = @import("gzip.zig");
 
 pub const StatusCode = enum(u32) {
     Ok = 200,
@@ -56,12 +57,42 @@ pub const Respond = struct {
     }
 
     pub fn addBody(allocator: std.mem.Allocator, stream: std.net.Stream, gzipEncode: bool, contentType: []const u8, content: []const u8) !void {
+        var content1 = content;
+        var len = content.len;
         if (gzipEncode) {
             try addHeader(stream, "Content-Encoding", "gzip");
+            var compressed_buffer: [1024]u8 = undefined;
+            var input_reader = std.Io.Reader.fixed(content);
+            var fixed_writer = std.Io.Writer.fixed(&compressed_buffer);
+            try gzip.compress(&input_reader, &fixed_writer, .{});
+            try fixed_writer.flush();
+            len = fixed_writer.end;
+            content1 = &compressed_buffer;
         }
         try addHeader(stream, "Content-Type", contentType);
-        try addHeader(stream, "Content-Length", try std.fmt.allocPrint(allocator, "{d}", .{content.len}));
+        try addHeader(stream, "Content-Length", try std.fmt.allocPrint(allocator, "{d}", .{len}));
         _ = try stream.write("\r\n");
-        _ = try stream.write(content);
+        _ = try stream.write(content1);
     }
 };
+
+// const CompressionResult = struct { len: usize, content: []const u8 };
+
+//     fn gzipString(content: []const u8) !CompressionResult {
+//         var compressed_buffer: [1024]u8 = undefined;
+//         var fixed_writer = std.Io.Writer.fixed(&compressed_buffer);
+//
+//         var input_buffer: [1024]u8 = undefined;
+//         @memcpy(input_buffer[0..content.len], content);
+//         var input_reader = std.Io.Reader.fixed(input_buffer[0..content.len]);
+//
+//         try gzip.compress(&input_reader, &fixed_writer, .{});
+//
+//         // Find the end of compressed data by checking for non-zero bytes
+//         var written: usize = 0;
+//         for (compressed_buffer, 0..) |byte, i| {
+//             if (byte != 0) written = i + 1;
+//         }
+//         return .{ .len = written, .content = compressed_buffer[0..written] };
+//     }
+// };
